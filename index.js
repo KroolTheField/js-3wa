7b00ab5d95327271677acb249b47ed993544959e
KroolTheField <l.pierrot.fr@outlook.fr>
2023-11-13 11:12:49 +0100

    Exercice 2

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ function myMap(arr, func) {
 
     return newArr
 }
+
+function myFilter(arr,func) {
+    const filteredArr = []
+
+    for (i=0; i < arr.length; i++) {
+        const result = func(arr[i])
+
+         if (result === true) {
+            filteredArr.push(result)
+         }
+    }
+
+    return filteredArr
+}
